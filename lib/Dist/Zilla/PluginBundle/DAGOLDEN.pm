5cf8f73682c63444c0dabe17cfece388b2bbaf8c
David Golden <dagolden@cpan.org>
2012-02-14 20:17:13 +0000

    Import of DAGOLDEN/Dist-Zilla-PluginBundle-DAGOLDEN-0.027 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-PluginBundle-DAGOLDEN
    gitpan-cpan-version:      0.027
    gitpan-cpan-path:         DAGOLDEN/Dist-Zilla-PluginBundle-DAGOLDEN-0.027.tar.gz
    gitpan-cpan-author:       DAGOLDEN
    gitpan-cpan-maturity:     released

--- lib/Dist/Zilla/PluginBundle/DAGOLDEN.pm
+++ lib/Dist/Zilla/PluginBundle/DAGOLDEN.pm
@@ -1,7 +1,7 @@
 use strict;
 use warnings;
 package Dist::Zilla::PluginBundle::DAGOLDEN;
-our $VERSION = '0.026'; # VERSION
+our $VERSION = '0.027'; # VERSION
 
 # Dependencies
 use autodie 2.00;
@@ -259,7 +259,7 @@ Dist::Zilla::PluginBundle::DAGOLDEN - Dist::Zilla configuration the way DAGOLDEN
 
 =head1 VERSION
 
-version 0.026
+version 0.027
 
 =head1 SYNOPSIS
 
